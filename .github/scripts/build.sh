5a390974f0bb7921b55479f49dbb1cc17f3d07fb
Ken Lauer <152229072+ken-lauer@users.noreply.github.com>
2025-06-30 09:42:09 -0700

    CI: add system library build as well

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -4,7 +4,7 @@ set -xe
 
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Wall -Wextra $CMAKE_CXX_FLAGS"
 
-make -j $(($(nproc) - 1)) -C build
+make -j -C build
 
 ls -la build/genesis4
 
