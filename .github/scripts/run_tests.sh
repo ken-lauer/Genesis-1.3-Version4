b0d9c06a65639a3a64b8b91f1b1cb8c3ed0bac37
Ken Lauer <152229072+ken-lauer@users.noreply.github.com>
2025-06-30 09:54:29 -0700

    CI: simple check for genesis4 output

--- .github/scripts/run_tests.sh
+++ .github/scripts/run_tests.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+output=$(./build/genesis4)
+if [[ "$output" != *"Usage: genesis4"* ]]; then
+  echo "Error: Expected 'Usage: genesis4' in output but got: $output"
+  exit 1
+else
+  echo "Genesis4 seems to have built correctly; 'usage' found in output."
+fi
+
 set -xe
 
 echo "(Placeholder for actual tests)"
-
-./build/genesis4
